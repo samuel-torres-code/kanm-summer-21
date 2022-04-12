4868a232c0d1d50504c4b54648001eb5c9cd7ab3
Urjeet Shrestha <42304832+ushrestha57@users.noreply.github.com>
2022-04-11 19:44:58 -0500

    added sqlalchemy that uses sqllite and created database models

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-
+app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
+db = SQLAlchemy(app)
 
 # Members API Route
 @app.route("/members")
